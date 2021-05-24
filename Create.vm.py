4ea15a5b41e370076dd97682e1bc26c8be60512b
Raihan Chowdhury <62661424+RaihanEXE99@users.noreply.github.com>
2021-05-24 18:50:52 +0600

    Create Create.vm.py

---
+++ Create.vm.py
@@ -0,0 +1,35 @@
+import libvirt
+
+pool = "default"
+
+conn = libvirt.open("qemu+ssh://raihan@51.79.160.118/system")
+if not conn:
+    raise SystemExit("Failed to open connection to qemu:///system")
+
+pool = conn.storagePoolLookupByName(pool)
+if not pool:
+    raise SystemExit("Failed to locate any StoragePool objects.")
+
+
+stgvol_xml = """
+<volume>
+  <name>{}</name>
+  <allocation>{}</allocation>
+  <capacity unit="M">{}</capacity>
+  <target>
+    <path>/var/lib/libvirt/images/sparse.img</path>
+    <permissions>
+      <owner>107</owner>
+      <group>107</group>
+      <mode>0744</mode>
+      <label>virt_image_t</label>
+    </permissions>
+  </target>
+</volume>""".format("hello","0","20")
+
+
+stgvol = pool.createXML(stgvol_xml, 0)
+if not stgvol:
+    raise SystemExit("Failed to create a  StorageVol objects.")
+
+conn.close()
