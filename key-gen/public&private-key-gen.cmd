97046c588dea97f91b49b3895b5620b8c6957caf
Raihan Chowdhury <62661424+RaihanEXE99@users.noreply.github.com>
2021-05-24 21:29:52 +0600

    Update public&private-key-gen.cmd

--- key-gen/public&private-key-gen.cmd
+++ key-gen/public&private-key-gen.cmd
@@ -1,1 +1,20 @@
+## S W TO PKEY U C ACCESS TM
+ssh-keygen -t rsa -b 4096
 
+# Check Keys
+cd ~/.ssh
+
+#send keys to remote server
+scp ~/.ssh/id_rsa.pub uname@host:/home/uname/.ssh/id_rsa.pub
+
+
+#REMOTE MECHINE
+cat ~/.ssh/id_rsa.pub >> authorized_keys
+
+# Check
+cat ~/.ssh/authorized_keys
+
+# SET PERMISSIONS
+chmod 700 ~/.ssh/
+
+chmod 600 ~/.ssh/*
