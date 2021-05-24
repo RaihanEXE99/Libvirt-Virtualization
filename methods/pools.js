1f87672765945ec626dab0539a8d1a033d15886a
Raihan Chowdhury <62661424+RaihanEXE99@users.noreply.github.com>
2021-05-24 18:43:03 +0600

    Update and rename pools.json to pools.js

--- methods/pools.js
+++ methods/pools.js
@@ -13,5 +13,17 @@
         'name', 'numOfVolumes', 'refresh', 
         'setAutostart', 'storageVolLookupByName', 
         'undefine'
-    ]
+    ],
+    "volume":[
+        'XMLDesc', '__class__', '__del__', '__delattr__', '__dict__', '__dir__',
+        '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__',
+        '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__',
+        '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
+        '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_conn', '_o',
+        'c_pointer', 'connect', 'delete', 'download', 
+        'info', 'infoFlags',
+        'key', 'name', 'path',
+        'resize', 'storagePoolLookupByVolume',
+        'streamBufSize', 'upload', 'wipe', 'wipePattern'
+     ]
 }
