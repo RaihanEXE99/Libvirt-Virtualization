06e32f6f0660f78cf651bd3276ee200d44aa6d01
Raihan Chowdhury <62661424+RaihanEXE99@users.noreply.github.com>
2021-05-31 18:46:34 +0600

    Update live.cmd

--- virt-manager/live.cmd
+++ virt-manager/live.cmd
@@ -1,1 +1,7 @@
 https://gist.github.com/ishad0w/788555191c7037e249a439542c53e170
+
+sudo apt-get install virt-manager
+sudo apt update
+sudo apt install qemu-kvm libvirt-daemon-system
+sudo adduser $USER libvirt
+newgrp libvirt
