c426bf44e9c5e5dc5a072f370bc9c98c29298433
Raihan Chowdhury <62661424+RaihanEXE99@users.noreply.github.com>
2021-07-16 17:11:33 +0600

    Update and rename screen.sh to s-links.sh

---
+++ vps/s-links.sh
@@ -0,0 +1,4 @@
+screen
+https://www.digitalocean.com/community/tutorials/how-to-install-and-use-screen-on-an-ubuntu-cloud-server
+nginx
+https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
--- vps/screen.sh
+++
@@ -1,1 +0,0 @@
-https://www.digitalocean.com/community/tutorials/how-to-install-and-use-screen-on-an-ubuntu-cloud-server
